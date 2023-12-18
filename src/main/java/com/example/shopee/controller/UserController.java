1cb62177f68bb29be3bc1771868a7362b7fc92fd
CQUAN <cuuquan2703@gmail.com>
2023-12-18 10:52:22 +0700

    fix: autowired repository

--- src/main/java/com/example/shopee/controller/UserController.java
+++ src/main/java/com/example/shopee/controller/UserController.java
@@ -7,21 +7,19 @@ import com.example.shopee.enity.User;
 import com.example.shopee.repository.UserRepository;
 import com.example.shopee.util.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
+@Controller
 public class UserController {
 
+    @Autowired
     private UserRepository repository;
     @Autowired
     private UserMapper mapper;
 
-    @GetMapping("/{id}")
-    public UserDto getUserById(@PathVariable Long id){
-        User user = repository.findById(id).orElseThrow(() -> new RuntimeException("User not exist"));
-        return mapper.entityToDto(user);
-    }
 
     @PutMapping("/new")
     public UserDto newUser() {
@@ -29,7 +27,10 @@ public class UserController {
         user.setUsername("cquan");
         user.setPassword("cquan");
         user.setRole(USER_ROLE.NORMAL);
+        System.out.println(user);
+        System.out.println(mapper.entityToDto(user));
         repository.save(user);
+
         return mapper.entityToDto(user);
     }
 }
