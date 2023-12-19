f6abfd59fd54ba7dc8fa2ae3a241fb3526e9d3c1
cuongha0701 <cuongha0701@gmail.com>
2023-12-19 09:29:06 +0700

    Field injection > Contructor injection

--- src/main/java/com/example/shopee/security/jwt/AuthTokenFilter.java
+++ src/main/java/com/example/shopee/security/jwt/AuthTokenFilter.java
@@ -9,7 +9,6 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -19,13 +18,17 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import com.example.shopee.security.services.UserDetailsServiceImpl;
 
 public class AuthTokenFilter extends OncePerRequestFilter {
-  @Autowired
   private JwtUtils jwtUtils;
-
-  @Autowired
   private UserDetailsServiceImpl userDetailsService;
-
   private static final Logger logger = LoggerFactory.getLogger(AuthTokenFilter.class);
+
+  public AuthTokenFilter(JwtUtils jwtUtils, UserDetailsServiceImpl userDetailsService) {
+    this.jwtUtils = jwtUtils;
+    this.userDetailsService = userDetailsService;
+  }
+
+  public AuthTokenFilter() {
+  }
 
   @Override
   protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
