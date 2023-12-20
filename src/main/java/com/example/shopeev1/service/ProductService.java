a796934e57551430c41d4701c79acad51661af93
dinhdiepvuong <diepvuong.py@gmail.com>
2023-12-20 13:31:34 +0700

    update, findbyId product

---
+++ src/main/java/com/example/shopeev1/service/ProductService.java
@@ -0,0 +1,14 @@
+package com.example.shopeev1.service;
+
+import com.example.shopeev1.data.Product;
+
+import java.util.List;
+
+public interface ProductService {
+
+    List<Product> getAll();
+
+    Product saveProduct(Product product);
+
+    Product findById(Long id);
+}
